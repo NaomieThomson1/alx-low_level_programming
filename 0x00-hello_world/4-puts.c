1f2bbcd6e49c9c1efba6ada9ced31ab4e8608bd4
NaomieThomson1 <naomiethomson1@gmail.com>
2022-06-30 19:45:50 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/
+
+ * main - Entry point
+ *Return: Always 0 (sucess)
+ */
 int main(void)
 {
-puts("\"programming is like building a multiligual puzzle");
+
+puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
+
 }

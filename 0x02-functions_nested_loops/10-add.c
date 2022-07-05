c431bed0dd5d64447d8308868e077364d630a30d
NaomieThomson1 <naomiethomson1@gmail.com>
2022-07-05 15:44:59 -0700

    nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,8 @@
 
 /**
  * add - check main
+ * @a: a is an integer
+ * @b: b is an integer
  * Description: function that adds two integers and return the result
  * Return: results of adding a and b
  */

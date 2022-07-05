bcfc9d3577977dc8996092be15f69ae4c70dcdd2
NaomieThomson1 <naomiethomson1@gmail.com>
2022-07-05 08:10:38 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - No entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char blade;
+	int a;
+
+	for (a = 0; a <= 0; a++)
+	{
+		_putchar(blade);
+	}
+	_putchar('\n');
+}

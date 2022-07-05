92150dc70adbcf82d5309c0334a92051df515a5b
NaomieThomson1 <naomiethomson1@gmail.com>
2022-07-05 14:39:12 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - check main
+ * @b: an intefer input
+ * Dscription: this function returns absolute value of a numberr
+ * Return: absolute value of number b
+ */
+
+int _abs(int b)
+{
+	if (b >= 0)
+		return (b);
+	else
+		return (b * -1);
+}
